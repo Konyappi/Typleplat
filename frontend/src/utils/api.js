b59c3e7e6a5ce1173816d9c79bb61924eda6aa21
Naufal <nfiraass78@gmail.com>
2026-08-28 13:29:39 +0800

    Connect frontend to Render backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://typleplate.onrender.com/api';
 
 /**
  * Fetches next license plate challenge from Python Flask backend with difficulty filter.
